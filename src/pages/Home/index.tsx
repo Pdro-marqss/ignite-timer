4fa931c619cf094397c00389194f11a59f46869b
Pedro Marques <pdro.marqs2015@hotmail.com>
2023-10-06 23:13:21 -0300

    form validation with zod

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,52 @@
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as zod from 'zod';
+
 import { Play } from 'phosphor-react';
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from './styles';
 
+const newCycleFormValidationSchema = zod.object({
+   task: zod.string().min(1, 'Informe a tarefa'),
+   minutesAmount: zod.number()
+      .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+      .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
+
+// interface NewCycleFormData {
+//    task: string;
+//    minutesAmount: number;
+// } A FORMA DE BAIXO TAMBEM FUNCIONA, MAS É USANDO ZOD
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
+   // register cria e da propriedades ao input e rrecebe o nome dele como parametro em string / handleSubmit recebe uma função handle como arg e devolve em data os dados recebidos dos campos do form ao realizar o submit / watch observa algum campo e mantem seu valor atualizado / formState guarda um valor de errors para gerar mensagens de erro na validação.
+   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+      resolver: zodResolver(newCycleFormValidationSchema),
+      defaultValues: {
+         task: '',
+         minutesAmount: 0,
+      }
+   });
+
+   function handleCreateNewCycle(data: NewCycleFormData) {
+      console.log(data);
+      reset(); //ele reseta os valores do input pro valor padrao definidos no defaultValues do useForm acima.
+   }
+
+   const task = watch('task');
+   const isSubmitDisabled = !task;
+
    return (
       <HomeContainer>
-         <form action="">
-            <FormContainer>
+         <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
+            <FormContainer>''
                <label htmlFor="task">Vou trabalhar em</label>
                <TaskInput
                   id="task"
-                  placeholder='De um nome para o seu projeto'
                   list='task-suggestions'
+                  placeholder='De um nome para o seu projeto'
+                  {...register('task')}
                />
                <datalist id='task-suggestions'>
                   <option value="Projeto 1" />
@@ -27,6 +63,7 @@ export function Home() {
                   step={5}
                   min={5}
                   max={60}
+                  {...register('minutesAmount', { valueAsNumber: true })}
                />
 
                <span>minutos.</span>
@@ -41,7 +78,7 @@ export function Home() {
             </CountdownContainer>
 
 
-            <StartCountdownButton disabled type="submit">
+            <StartCountdownButton disabled={isSubmitDisabled} type="submit">
                <Play size={24} />
                Começar
             </StartCountdownButton>
