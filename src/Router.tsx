6923d770a29703492f469b9cd3d03f900800ed0b
Pedro Marques <pdro.marqs2015@hotmail.com>
2025-05-07 23:00:08 -0300

    [FEAT] Configurando layout de rota
    Configuração base de layout de rotas usando Outlet / Criando componente Header somente pra testes

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { History } from './pages/History';
+import { DefaultLayout } from './layouts/Default.layout';
 
 export function Router() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/history' element={<History />} />
+      {/* Aplica pra todas as rotas o layout default e aloca o conteudo no <Outlet /> do componente */}
+      <Route path='/' element={<DefaultLayout />} >
+        <Route path='/' element={<Home />} />
+        <Route path='/history' element={<History />} />
+      </Route>
     </Routes>
   );
 }
